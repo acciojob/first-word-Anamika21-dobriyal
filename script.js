910c9c35f3202c58d3309b0e4cc3e087ea2f82da
Anamika <anamikadobriyal21@gmail.com>
2024-08-17 19:31:11 +0530

    created a submission

--- script.js
+++ script.js
@@ -5,9 +5,8 @@ function firstWord(s) {
     if (s === '') return '';
     const spaceIndex = s.indexOf(' ');
     return spaceIndex === -1 ? s : s.substring(0, spaceIndex);
-
 }
 
-// Do not change the code below
+// Do not change the code below  
 const s = prompt("Enter String:");
 alert(firstWord(s));
